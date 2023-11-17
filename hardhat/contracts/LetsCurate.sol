bdeb80f93227244ec778173d14d1112fa52e4370
Ferit <ferit@cryptolab.net>
2023-11-17 13:17:18 +0300

    policy creation logic

--- hardhat/contracts/LetsCurate.sol
+++ hardhat/contracts/LetsCurate.sol
@@ -1,15 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import 'hardhat/console.sol';
+import "hardhat/console.sol";
 
 contract LetsCurate {
-  struct Item {
-    address payable publisher;
-    uint8 curationPolicyCode;
-  }
+    uint256 public curationPolicyCounter;
 
-  constructor() {
-    console.log('Deploying a LetsCurate contract');
-  }
+    struct Item {
+        address payable publisher;
+        uint16 curationPolicyCode;
+    }
+
+    event CurationPolicy(uint256 indexed curationPolicyCode, string policy);
+
+    /// @notice Create a new curation policy.
+    /// @param curationPolicy IPFS content identifier of the curation policy.
+    function newCurationPolicy(string memory curationPolicy) public {
+        require(
+            curationPolicyCounter + 1 != 0,
+            "No space left for a new curation policy"
+        );
+
+        emit CurationPolicy(curationPolicyCounter, curationPolicy);
+
+        curationPolicyCounter++;
+    }
+
+    constructor() {
+        console.log("Deploying a LetsCurate contract");
+    }
 }
